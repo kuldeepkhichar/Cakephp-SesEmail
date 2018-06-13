df6aa84bc929810dd9833cb10e214a60aa959269
Kuldeep Choudhary <erkuldeepkhichar@gmail.com>
2018-06-13 17:55:46 +0530

    Update ApiTransport.php

--- Ses/Network/Email/ApiTransport.php
+++ Ses/Network/Email/ApiTransport.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/*
  * Aws Api class
  *
  * Enables sending of email from Cakephp 2 over SES via AWS SDK for PHP - Version 3
@@ -17,50 +17,50 @@
 App::import('Vendor', 'aws', array('file' => 'aws'. DS . 'aws-autoloader.php'));
 
 class ApiTransport extends AbstractTransport {
-	
-	protected $_config = array();
-	
-	/*
-		@params CakeEmail $email
-		@return array('ses_message_id' => 'message_id')
-	*/
-	public function send(CakeEmail $email) {
-		$sesEmail = new \Aws\Ses\SesClient(array(
-			'credentials' => array(
-				'key' => $this->_config['key'],
-				'secret' => $this->_config['secret'],
-			),
-			'region' => $this->_config['region'],
-			'version' => '2010-12-01'
-		));
-		
-		$headers = $email->getHeaders(array('from', 'sender', 'replyTo', 'readReceipt', 'returnPath', 'to', 'cc', 'subject'));
-		$headers = $this->_headersToString($headers);
-		
-		$lines = $email->message();
-		$messages = array();
-		foreach ($lines as $line) {
-			if ((!empty($line)) && ($line[0] === '.')) {
-				$messages[] = '.' . $line;
-			}
-			else {
-				$messages[] = $line;
-			}
-		}
-		
-		$message = implode("\r\n", $messages);
-		$complete_message = $headers . "\r\n\r\n" . $message . "\r\n\r\n\r\n";
-		
-		try {
-			$result = $sesEmail->sendRawEmail(array(
-				'RawMessage' => array(
-					'Data' => $complete_message
-				)
-			));
-			return array('ses_message_id' => $result->get('MessageId'));
-		}
-		catch (MessageRejectedException $e) {
-			return false;
-		}
-	}
+    
+    protected $_config = array();
+    
+    /*
+        @params CakeEmail $email
+        @return array('ses_message_id' => 'message_id')
+    */
+    public function send(CakeEmail $email) {
+        $sesEmail = new \Aws\Ses\SesClient(array(
+            'credentials' => array(
+                'key' => $this->_config['key'],
+                'secret' => $this->_config['secret'],
+            ),
+            'region' => $this->_config['region'],
+            'version' => '2010-12-01'
+        ));
+        
+        $headers = $email->getHeaders(array('from', 'sender', 'replyTo', 'readReceipt', 'returnPath', 'to', 'cc', 'subject'));
+        $headers = $this->_headersToString($headers);
+        
+        $lines = $email->message();
+        $messages = array();
+        foreach ($lines as $line) {
+            if ((!empty($line)) && ($line[0] === '.')) {
+                $messages[] = '.' . $line;
+            }
+            else {
+                $messages[] = $line;
+            }
+        }
+        
+        $message = implode("\r\n", $messages);
+        $complete_message = $headers . "\r\n\r\n" . $message . "\r\n\r\n\r\n";
+        
+        try {
+            $result = $sesEmail->sendRawEmail(array(
+                'RawMessage' => array(
+                    'Data' => $complete_message
+                )
+            ));
+            return array('ses_message_id' => $result->get('MessageId'));
+        }
+        catch (MessageRejectedException $e) {
+            return false;
+        }
+    }
 }
